46ceeecffd4c3872471f3094cdcef1bbc075129c
MaksStelmakh <step4ick2017@gmail.com>
2021-10-15 14:33:36 +0300

    1 question

---
+++ src/js/task-01.js
@@ -0,0 +1,20 @@
+// const categoriesEl = document.querySelectorAll(`.item`);
+
+// const categoryEl = document.querySelectorAll(`h2`);
+
+// const totalElFirstCategories = categoriesEl[0].querySelectorAll(`li`)
+// const totalElSecondCategories = categoriesEl[1].querySelectorAll(`li`)
+// const totalElThridCategories = categoriesEl[2].querySelectorAll(`li`)
+
+
+
+// console.log(`Number of categories: ${categoriesEl.length}`);
+
+// console.log(`Category: ${categoryEl[0].textContent}`)
+// console.log(`Elements: ${totalElFirstCategories.length}`)
+
+// console.log(`Category: ${categoryEl[1].textContent}`)
+// console.log(`Elements: ${totalElSecondCategories.length}`)
+
+// console.log(`Category: ${categoryEl[2].textContent}`)
+// console.log(`Elements: ${totalElThridCategories.length}`)
