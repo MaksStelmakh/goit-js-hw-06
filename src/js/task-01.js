8bd9c813e8fce238e069d53d1df81bc412b4a548
MaksStelmakh <step4ick2017@gmail.com>
2021-10-16 11:23:43 +0300

    1 Task

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,20 +1,8 @@
-// const categoriesEl = document.querySelectorAll(`.item`);
+const categoriesEl = document.querySelectorAll(`.item`);
 
-// const categoryEl = document.querySelectorAll(`h2`);
+console.log(`Number of categories: ${categoriesEl.length}`);
 
-// const totalElFirstCategories = categoriesEl[0].querySelectorAll(`li`)
-// const totalElSecondCategories = categoriesEl[1].querySelectorAll(`li`)
-// const totalElThridCategories = categoriesEl[2].querySelectorAll(`li`)
-
-
-
-// console.log(`Number of categories: ${categoriesEl.length}`);
-
-// console.log(`Category: ${categoryEl[0].textContent}`)
-// console.log(`Elements: ${totalElFirstCategories.length}`)
-
-// console.log(`Category: ${categoryEl[1].textContent}`)
-// console.log(`Elements: ${totalElSecondCategories.length}`)
-
-// console.log(`Category: ${categoryEl[2].textContent}`)
-// console.log(`Elements: ${totalElThridCategories.length}`)
+categoriesEl.forEach(elem => {
+    console.log(`Category: ${elem.firstElementChild.textContent}`)
+    console.log(`Elements: ${elem.lastElementChild.children.length}`)
+})
