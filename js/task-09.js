f45225a09bef748763dac3210b3975aacced818e
MaksStelmakh <step4ick2017@gmail.com>
2021-10-16 22:27:34 +0300

    9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const background = document.querySelector(`body`)
+const buttonChangeColor = document.querySelector(`.change-color`)
+const spanResultColor = document.querySelector(`.color`)
+
+buttonChangeColor.addEventListener(`click`, changeColorFunc)
+
+function changeColorFunc() {
+  const changeColor = getRandomHexColor()
+  spanResultColor.textContent = changeColor
+  console.log(background.style.backgroundColor = changeColor)
+
+}
